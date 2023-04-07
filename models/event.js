401bab07972c8c10b75bc4d014be903166f73750
Prakhar-Verma39 <103757447+Prakhar-Verma39@users.noreply.github.com>
2023-04-07 22:17:54 +0530

    adding local pages

---
+++ models/event.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+
+const eventSchema = new mongoose.Schema({
+    title:{
+        type: String,
+        required: true
+    },
+    description:{
+        type: String,
+        required: true
+    },
+    pdfUrl:{
+        type: String,
+        required: true
+    }
+    // ,
+    // date: {
+    //     type: date,
+    //     required: true,
+    // }
+})
+
+const Event = mongoose.model('Event', eventSchema); 
+
+module.exports = Event;
